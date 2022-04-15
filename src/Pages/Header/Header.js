7c3a2d5e0674306142343315fdd29f55f9f5328d
Eliyas Hossain <eliyashossain700@gmail.com>
2022-04-15 15:34:19 +0600

    Header Section Completed

---
+++ src/Pages/Header/Header.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import logo from "../../images/logo2.png";
+import { FiShoppingCart } from "react-icons/fi";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <>
+      <Navbar bg="light" expand="lg">
+        <Container fluid className="nav-bar">
+          <Navbar.Brand href="#">
+            <img height={30} src={logo} alt="" />{" "}
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="text-right nav-items my-lg-0 ms-auto"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link>
+                <Link to="/cart">
+                  <FiShoppingCart
+                    size={34}
+                    className=" text-center mt-lg-2 mx-3 cart"
+                  />
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/login" className="navlink py-2 ">
+                  Log In
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/signup" className="navlink py-2 ">
+                  Sign Up
+                </Link>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default Header;
